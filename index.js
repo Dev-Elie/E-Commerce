472c6d9a8ca3a269d910bd7a2a6f790997aa2e66
Dev-Elie <eochieng9448@gmail.com>
2022-03-14 18:01:58 +0300

    Added user authentication with AT

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = express();
 
 const moongose = require("mongoose")
 const dotenv = require("dotenv")
+
 const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+
 
 dotenv.config();
 
@@ -14,7 +18,9 @@ moongose.connect(process.env.MONGO_URL).then(() => {
     console.log(error)
 })
 app.use(express.json())
-app.use("/api/v1.1/users", userRoute)
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server is up")
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,47 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const {verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken")
 
-router.get("/test", (req, res)=>{
-    res.send("User test success")
+// UPDATE 
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC).toString()
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true })
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
-router.post("/user", (req, res)=>{
-    const username = req.body.username
-    res.send("Your username is " +username)
+// DELETE
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted");
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+// GET USER
+
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        const {password, ...others } = user._doc;
+        res.status(200).json(others)
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 module.exports = router
